0ca2de13acaaa3e49af2dd8a3fb1189fb8341268
Clemens Hoffmann <cannelony@googlemail.com>
2013-01-07 16:14:02 +0100

    add testcase

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 // read file
 try {
-  var data = fs.readFileSync('data2.json', 'ascii');
+  var data = fs.readFileSync('lod2.eu.json', 'ascii');
   console.log('Input: ', data);
 } catch (err) {
   console.error("There was an error while opening the file:");
@@ -17,15 +17,15 @@ console.log('Successfully insert: ', msg);
 
 // hasSubject
 var start = process.hrtime();
-var hasSubject = dstore.hasSubject("http://example.org/about", function(err, status) {
+var hasSubject = dstore.hasSubject("http://seebi.myopenid.com/", function(err, status) {
   var stop = process.hrtime();
 	console.log('HasSubject took ' + (stop[1] - start[1])/1000000 + ' ms!');
-	console.log("\nhttp://example.org/about is already inserted:  " + status + '\n');
+	console.log("\nhttp://seebi.myopenid.com/ is already inserted:  " + status + '\n');
 });
 
 // find
 var start = process.hrtime();
-var find = dstore.find("http://example.org/about", function(err, result) {
+var find = dstore.find("http://seebi.myopenid.com/", function(err, result) {
   var stop = process.hrtime();
   console.log('Enter callback \"find\"');
   console.log('Query took ' + (stop[1] - start[1])/1000000 + ' ms!');
